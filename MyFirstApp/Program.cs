a35f4b434634113891d9d1d519bea700fc4f0732
bishnulimbu <subashlimbu987654321@gmail.com>
2024-07-01 09:59:42 +0545

    sum c# program

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,6 @@
+﻿    Console.Write("enter the first number:");
+    int fNum = Convert.ToInt32(Console.ReadLine());
+    Console.Write("enter the second number:");
+    int sNum = Convert.ToInt32(Console.ReadLine());
+    int sum = fNum + sNum;
+    Console.WriteLine("sum" +" : "+ sum);
