de5fd2ada06764e3530b223c07e8965251a56333
bishnulimbu <subashlimbu987654321@gmail.com>
2024-07-07 08:06:53 +0545

    reversing the string

---
+++ reverseString.cs
@@ -0,0 +1,20 @@
+using System;
+class Program{
+  static void Main(){
+    string str= "bishnu";
+    reverse(ref str);
+Console.WriteLine(str);
+
+  }
+  static void reverse(ref string one){
+    char[] charArray = one.ToCharArray();
+    Array.Reverse(charArray);
+    one = new String(charArray);
+  }
+}
+//string b = new StringBuilder(" ");
+// for(int i= word.length - 1; i >= 0;i--){
+//  b.Append(word[i]);
+// }
+// return b.ToString();
+//three types of reverse string technique: 1. using string builder, 2. uing character and 3. using Array.Reverse(array) method.
