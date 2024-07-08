19ae3cc30d3648efbcb959ce4a5e5f275b7f5ffd
bishnulimbu <subashlimbu987654321@gmail.com>
2024-07-08 07:51:09 +0545

    curretly fibo number generator

---
+++ fibo.cs
@@ -0,0 +1,25 @@
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter the number for fibonacci");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            System.Console.WriteLine(fibo(n) + " ");
+
+        }
+        fibo(n);
+
+
+    }
+    static void fibo(int n);
+    {
+      int sum;
+}
+}
+
+
+//n number of fibonacci using recursion
