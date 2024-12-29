6d7129db33fcbb4d361300a3db50e55b52a599e5
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-12-29 14:35:38 +0545

    hello file for dotnet-script

---
+++ assignment/hello.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("hello");
+        hello();
+    }
+
+    static void hello()
+    {
+        Console.WriteLine("Hello from hello()");
+    }
+}
