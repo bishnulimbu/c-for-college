3ad23bacaf2dae045231b202b25d2e4c01d2446a
Bishnu Limbu <subashlimbu987654321@gmail.com>
2025-01-01 22:29:06 +0545

    generics example

--- examPract/Program.cs
+++ examPract/Program.cs
@@ -1,24 +1,16 @@
 ﻿using System;
 
-class AgeException:Exception{
-    public AgeException(string message){
-    :base(message);
-    }
-}
-
 public class Program
 {
+    public static void print<T>(T message)
+    {
+        Console.WriteLine(message);
+    }
+
     public static void Main(string[] args)
     {
-        try
-        {
-            int age = 11;
-            if (age <= 18)
-                throw new Exception("age is not valid.");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Exception: " + e.Message);
-        }
+        print(111);
+        print("hello");
+        print(222);
     }
 }
