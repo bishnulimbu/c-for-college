b9843e85c50095c97a520ab76e42d7e8fcb6250f
Bishnu Limbu <subashlimbu987654321@gmail.com>
2025-01-01 12:40:58 +0545

    linq

--- assignment/linq(where).cs
+++ assignment/linq(where).cs
@@ -1,11 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class Progarm
 {
     public static void Main()
     {
-        int[] numbers = { 1, 2, 3, 4, 5 };
+        // int[] numbers = { 1, 2, 3, 4, 5 };
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
 
         var evenNum = numbers.Where(n => n % 2 == 0);
         foreach (int val in evenNum)
