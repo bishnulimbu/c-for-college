b5fa335059ce4a611ddb124d138db9cbab0d1094
Bishnu Limbu <subashlimbu987654321@gmail.com>
2025-01-01 12:41:16 +0545

    main program cs file

---
+++ examPract/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class AgeException:Exception{
+    public AgeException(string message){
+    :base(message);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            int age = 11;
+            if (age <= 18)
+                throw new Exception("age is not valid.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+    }
+}
